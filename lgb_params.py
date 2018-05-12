3464d6c16a7dc6e5fd1096ee4e41ba0ea9a3e369
qxy <714842585@qq.com>
2018-05-12 15:38:10 +0800

    加入height特征,lgb调参

--- lgb_params.py
+++ lgb_params.py
@@ -67,13 +67,10 @@ for num_leaves in range(20, 200, 5):
     for max_depth in range(3, 8, 1):
         params['num_leaves'] = num_leaves
         params['max_depth'] = max_depth
-
-        cv_results = lgb.cv(params, lgb_train, seed=2018, nfold=3, metrics=['mse'], early_stopping_rounds=10,
-            verbose_eval=False)
-
+        cv_results = lgb.cv(params, lgb_train, seed=2018, nfold=3, metrics=['mse']
+                            , early_stopping_rounds=10,verbose_eval=None)
         mean_merror = pd.Series(cv_results['l2-mean']).min()
         boost_rounds = pd.Series(cv_results['l2-mean']).argmin()
-
         if mean_merror < min_merror:
             min_merror = mean_merror
             best_params['num_leaves'] = num_leaves
@@ -89,13 +86,11 @@ for max_bin in range(1, 255, 5):
     for min_data_in_leaf in range(10, 200, 5):
         params['max_bin'] = max_bin
         params['min_data_in_leaf'] = min_data_in_leaf
-
-        cv_results = lgb.cv(params, lgb_train, seed=42, nfold=3, metrics=['mse'], early_stopping_rounds=3,
-            verbose_eval=False)
+        cv_results = lgb.cv(params, lgb_train, seed=42, nfold=3, metrics=['mse']
+                            , early_stopping_rounds=3,verbose_eval=None)
 
         mean_merror = pd.Series(cv_results['l2-mean']).min()
         boost_rounds = pd.Series(cv_results['l2-mean']).argmin()
-
         if mean_merror < min_merror:
             min_merror = mean_merror
             best_params['max_bin'] = max_bin
@@ -113,13 +108,10 @@ for feature_fraction in [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]:
             params['feature_fraction'] = feature_fraction
             params['bagging_fraction'] = bagging_fraction
             params['bagging_freq'] = bagging_freq
-
-            cv_results = lgb.cv(params, lgb_train, seed=42, nfold=3, metrics=['mse'], early_stopping_rounds=3,
-                verbose_eval=False)
-
+            cv_results = lgb.cv(params, lgb_train, seed=42, nfold=3, metrics=['mse']
+                                , early_stopping_rounds=3,verbose_eval=None)
             mean_merror = pd.Series(cv_results['l2-mean']).min()
             boost_rounds = pd.Series(cv_results['l2-mean']).argmin()
-
             if mean_merror < min_merror:
                 min_merror = mean_merror
                 best_params['feature_fraction'] = feature_fraction
@@ -139,13 +131,10 @@ for lambda_l1 in [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]:
             params['lambda_l1'] = lambda_l1
             params['lambda_l2'] = lambda_l2
             params['min_split_gain'] = min_split_gain
-
-            cv_results = lgb.cv(params, lgb_train, seed=42, nfold=3, metrics=['mse'], early_stopping_rounds=3,
-                verbose_eval=False)
-
+            cv_results = lgb.cv(params, lgb_train, seed=42, nfold=3, metrics=['mse']
+                                , early_stopping_rounds=3,verbose_eval=None)
             mean_merror = pd.Series(cv_results['l2-mean']).min()
             boost_rounds = pd.Series(cv_results['l2-mean']).argmin()
-
             if mean_merror < min_merror:
                 min_merror = mean_merror
                 best_params['lambda_l1'] = lambda_l1
